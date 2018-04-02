81331a5610338ae3ec98fbb900a4f45f1d4a8da7
nguyentan11 <ntan1107@gmail.com>
2018-04-02 22:32:20 +0700

    Bai tap 2 tuan 4

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipserver = new IPEndPoint(IPAddress.Parse("192.168.1.108"),80);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            try
+            {
+                client.Connect(ipserver);
+                Console.WriteLine(ipserver.ToString() + " is opened.");
+                client.Close();
+            }
+            catch
+            {
+                Console.WriteLine(ipserver.ToString() + "is closed.");
+            }
+        }
+    }
+}
